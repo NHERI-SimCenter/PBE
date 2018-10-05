e2db45bf888f13151e5457703918004bc1bb0974
fmckenna <fmckenna@berkeley.edu>
2018-10-04 21:04:27 -0700

    fmk - fixing style sheet error on radius in main.cpp

--- main.cpp
+++ main.cpp
@@ -57,7 +57,7 @@ int main(int argc, char *argv[])
 
 
   theInputApp->setStyleSheet("QComboBox {background: #FFFFFF;} \
-QLineEdit {background-color: #FFFFFF;background-radius: 5px; border: 2px solid darkgray;} \
+QLineEdit {background-color: #FFFFFF; border: 2px solid darkgray;} \
 QTabWidget::pane {background-color: #ECECEC; border: 1px solid rgb(244, 244, 244);}");
 
   int res = a.exec();
