26bf6c7f0a60772923ddca6e004569680a93e1ca
fmckenna <fmckenna@berkeley.edu>
2020-02-03 12:29:09 -0800

    fmk - updating feedback to point to message board

--- main.cpp
+++ main.cpp
@@ -180,6 +180,8 @@ int main(int argc, char *argv[])
   QString manualURL("https://www.designsafe-ci.org/data/browser/public/designsafe.storage.community//SimCenter/Software/PBE");
   w.setDocumentationURL(manualURL);
 
+  QString messageBoardURL("https://simcenter-messageboard.designsafe-ci.org/smf/index.php?board=7.0");
+  w.setFeedbackURL(messageBoardURL);
 
   //
   // move remote interface to a thread
