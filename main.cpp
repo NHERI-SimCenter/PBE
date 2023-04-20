4461487c98b7ca1ab1cfa9145dbd6015a4eefe31
fmckenna <fmckenna@berkeley.edu>
2023-04-20 16:38:37 -0700

    fmk - updating version to 3.0.8

--- main.cpp
+++ main.cpp
@@ -56,7 +56,7 @@ int main(int argc, char *argv[])
     //Setting Core Application Name, Organization and Version
     QCoreApplication::setApplicationName("PBE");
     QCoreApplication::setOrganizationName("SimCenter");
-    QCoreApplication::setApplicationVersion("3.0.7");
+    QCoreApplication::setApplicationVersion("3.0.8");
     // GoogleAnalytics::SetTrackingId("UA-126256136-1");
     GoogleAnalytics::StartSession();
     GoogleAnalytics::ReportStart();
