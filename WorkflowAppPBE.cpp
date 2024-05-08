3f3ddfdd9245a804cba201a72898b2a0e22c505f
Adam Zsarnoczay <33822153+zsarnoczay@users.noreply.github.com>
2024-05-07 18:39:03 -0700

    azs - update main app citation
    Replaced one-line string with QJson objects to facilitate future edits.

--- WorkflowAppPBE.cpp
+++ WorkflowAppPBE.cpp
@@ -677,6 +677,39 @@ WorkflowAppPBE::createCitation(QJsonObject &citation, QString citeFile) {
   //
   // put PBE citation in
   // 
+
+  QJsonObject citationPBE;
+  citationPBE.insert("citation",
+"Adam Zsarnoczay, Frank McKenna, Charles Wang, Stevan Gavrilovic, Michael Gardner, \
+Sang-ri Yi, Aakash Bangalore Satish, & Wael Elhaddad. (2024). \
+NHERI-SimCenter/PBE: Version 3.4.0 (V3.4.0). \
+Zenodo. https://doi.org/10.5281/zenodo.10902085)");
+  citationPBE.insert("description",
+"This is the overall tool reference used to indicate the version of the tool.");
+
+  QJsonObject citationSimCenterMarker;
+  citationSimCenterMarker.insert("citation",
+"Gregory G. Deierlein, Frank McKenna, Adam Zsarnóczay, Tracy Kijewski-Correa, \
+Ahsan Kareem, Wael Elhaddad, Laura Lowes, Mat J. Schoettler, and Sanjay Govindjee (2020) \
+A Cloud-Enabled Application Framework for Simulating Regional-Scale Impacts of Natural \
+Hazards on the Built Environment. \
+Frontiers in the Built Environment. 6:558706. doi: 10.3389/fbuil.2020.558706");
+  citationSimCenterMarker.insert("description",
+"This marker paper describes the SimCenter application framework, which was designed \
+to simulate the impacts of natural hazards on the built environment. It is a \
+necessary attribute for publishing work resulting from the use of SimCenter tools, \
+software, and datasets.");
+
+  QJsonArray citationsArray;
+  citationsArray.push_back(citationPBE);
+  citationsArray.push_back(citationSimCenterMarker);
+
+  QJsonObject unnecessaryCitationsElement;
+  unnecessaryCitationsElement.insert("citations", citationsArray);
+
+  citation.insert("PBE",unnecessaryCitationsElement);
+
+  /*
   QString cit("{\"PBE\": { \"citations\": [{\"citation\": \"Adam Zsarnoczay, Frank McKenna, Charles Wang, Stevan Gavrilovic, Michael Gardner, Sang-ri Yi, Aakash Bangalore Satish, & Wael Elhaddad. (2024). NHERI-SimCenter/PBE: Version 3.4.0 (V3.4.0). Zenodo. https://doi.org/10.5281/zenodo.10902085\",\"description\": \"This is the overall tool reference used to indicate the version of the tool.\"},{\"citation\": \"Gregory G. Deierlein, Frank McKenna, Adam Zsarnóczay, Tracy Kijewski-Correa, Ahsan Kareem, Wael Elhaddad, Laura Lowes, Mat J. Schoettler, and Sanjay Govindjee (2020) A Cloud-Enabled Application Framework for Simulating Regional-Scale Impacts of Natural Hazards on the Built Environment. Frontiers in the Built Environment. 6:558706. doi: 10.3389/fbuil.2020.558706\",\"description\": \" This marker paper describes the SimCenter application framework, which was designed to simulate the impacts of natural hazards on the built environment.It  is a necessary attribute for publishing work resulting from the use of SimCenter tools, software, and datasets.\"}]}}");
 
   QJsonDocument docC = QJsonDocument::fromJson(cit.toUtf8());
@@ -688,6 +721,7 @@ WorkflowAppPBE::createCitation(QJsonObject &citation, QString citeFile) {
       qDebug() << "WorkflowdAppEE_UQ citation text is not valid JSON: \n" << cit << endl;
     }
   }
+  */
 
   theSIM_Selection->outputCitation(citation);
   theEventSelection->outputCitation(citation);
