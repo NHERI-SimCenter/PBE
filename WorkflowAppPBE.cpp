981ce771e25b242d07f8d9eb6637c7d01ac57fcc
fmckenna <fmckenna@berkeley.edu>
2024-10-01 16:14:30 -0700

    fmk - updating main and Workflow App with latest zenodo DOI

--- WorkflowAppPBE.cpp
+++ WorkflowAppPBE.cpp
@@ -800,7 +800,7 @@ WorkflowAppPBE::createCitation(QJsonObject &citation, QString citeFile) {
 
   QJsonObject citationPBE;
   citationPBE.insert("citation",
-		     "Adam Zsarnoczay, Frank McKenna, Kuanshi Zhong, Michael Gardner, Charles Wang, Sang-ri Yi, Aakash Bangalore Satish, Amin Pakzad, & Wael Elhaddad. (2024). NHERI-SimCenter/EE-UQ: Version 4.0.0 (v4.0.0). Zenodo. https://doi.org/10.5281/zenodo.13324153");
+		     "Adam Zsarnoczay, Frank McKenna, Michael Gardner, Michael Gardner, Charles Wang, Sang-ri Yi, Aakash Bangalore Satish, Amin Pakzad, & Wael Elhaddad. (2024). NHERI-SimCenter/PBE: Version 4.1.0 (v4.1.0). Zenodo. https://doi.org/10.5281/zenodo.13865401");
   citationPBE.insert("description",
 		     "This is the overall tool reference used to indicate the version of the tool.");
 
