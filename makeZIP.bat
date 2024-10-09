5ff8166aa1a34c43c5b3987519823b43808a596e
fmckenna <fmckenna@berkeley.edu>
2024-10-09 06:50:58 -0700

    fmk - updating makeZIP to include tools dir

--- makeZIP.bat
+++ makeZIP.bat
@@ -35,6 +35,7 @@ mkdir .\applications\Workflow
 mkdir .\applications\opensees
 mkdir .\applications\dakota
 mkdir .\applications\python
+mkdir .\applications\tools
 
 xcopy /s /e ..\..\..\SimCenterBackendApplications\applications\common  .\applications\common
 xcopy /s /e ..\..\..\SimCenterBackendApplications\applications\createEDP  .\applications\createEDP
@@ -49,6 +50,7 @@ xcopy /s /e ..\..\..\SimCenterBackendApplications\applications\Workflow  .\appli
 xcopy /s /e ..\..\..\SimCenterBackendApplications\applications\OpenSees  .\applications\opensees
 xcopy /s /e ..\..\..\SimCenterBackendApplications\applications\dakota  .\applications\dakota
 xcopy /s /e /Q ..\..\..\SimCenterBackendApplications\applications\python  .\applications\python
+xcopy /s /e ..\..\..\SimCenterBackendApplications\applications\tools  .\applications\tools
 
 rmdir /s /q .\applications\createEVENT\ASCE7_WindSpeed
 rmdir /s /q .\applications\createEVENT\CFDEvent
